41cc8ce62e512841cd8d639c6cfef4c7827065d4
Dhruvik shah <dhruvik9a03@gmail.com>
2022-11-29 11:48:42 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import FrontAni from './FrontAni'
+import './style/header.css'
+const Header = () => {
+    const list= [
+        {name: "About" , link: './'},
+        {name: "Contact" , link: './'},
+        {name: "Education" , link: './'},
+        {name: "Service" , link: './'}
+    ]
+  return (
+    <>
+    <FrontAni/>
+    <div className='pfHeader'>
+        <h1>Shah<span>Dhruvik</span></h1>
+        <div>
+            <button><img src="menu.svg" alt="menu"  /></button>
+            <nav>
+                <ul>
+                {list.map((item)=>{
+                    return <li key={item.name}><a href={item.link}>{item.name}</a></li>
+                })}
+                </ul>
+            </nav>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PortFolio from './PortFolio'
+import './index.css'
+import Header from './Header'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+  
+    <Header/>
+    {/* <PortFolio/> */}
+  </>
+)
