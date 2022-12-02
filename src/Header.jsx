0350237c04e3d0f03640b2f3d14050701fced060
Dhruvik shah <dhruvik9a03@gmail.com>
2022-12-02 17:53:00 +0530

    close remained

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,49 +12,49 @@ const Header = () => {
   const openNav = () => {
     setopenBtn(!openBtn)
   }
+  
   return (
     <>
       {/* <FrontAni /> */}
-      
-        <div className="pfHeader">
-          <div className="pfNameBtn">
-            <h1>
-              shah<span>Dhruvik</span>
-            </h1>
-            {openBtn == false && (
-              <button>
-                <img src="menu.svg" alt="menu" onClick={openNav} />
-              </button>
-            )}
-          </div>
-          <div className={` ${openBtn ? "pfNavClose pfNavCls" : "hidden"}`}>
-            <nav>
-              <ul>
-                <li>
-                  <a href="./">ABOUT</a>
-                </li>
-                <li>
-                  <a href="./">CONTACT</a>
-                </li>
-                <li>
-                  <a href="./">EDUCATION</a>
-                </li>
-                <li>
-                  <a href="./">SERVICE</a>
-                </li>
-              </ul>
-            </nav>
+
+      <div className="pfHeader">
+        <div className="pfNameBtn">
+          <h1>
+            shah<span>Dhruvik</span>
+          </h1>
+          {openBtn == false && (
             <button>
-              <img src="close.svg" alt="menu" onClick={openNav} />
+              <img src="menu.svg" alt="menu" onClick={openNav} />
             </button>
-          </div>
+          )}
         </div>
-        <div className="frontAni">
-          {/* Hello, I'm Dhruvik Shah */}
-          <p>Hello</p>
-          <p>I'm Dhruvik Shah</p>
+        <div className={` ${openBtn ? 'pfNavClose pfNavCls' : '  pfNavWeb '}  `}>
+          <nav>
+            <ul>
+              <li>
+                <a href="./">ABOUT</a>
+              </li>
+              <li>
+                <a href="./">CONTACT</a>
+              </li>
+              <li>
+                <a href="./">EDUCATION</a>
+              </li>
+              <li>
+                <a href="./">SERVICE</a>
+              </li>
+            </ul>
+          </nav>
+          <button>
+            <img src="close.svg" alt="menu" onClick={openNav} />
+          </button>
         </div>
-      
+      </div>
+      <div className="frontAni">
+        {/* Hello, I'm Dhruvik Shah */}
+        <p>Hello</p>
+        <p>I'm Dhruvik Shah</p>
+      </div>
     </>
   )
 }
